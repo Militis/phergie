947d8a9458ea3356d2c97b722563f813581f16a4
Militis <militis88@gmail.com>
2012-11-06 04:41:10 -0600

    Revert "Debugging YouTube plugin."
    This reverts commit 66b715d85a9c9e40b257ee4ba9ca89160a225780.

--- Phergie/Plugin/Youtube.php
+++ Phergie/Plugin/Youtube.php
@@ -73,7 +73,6 @@ class Phergie_Plugin_Youtube extends Phergie_Plugin_Abstract
         if (!$entries) {
             $url = $this->plugins->getPlugin('Url');
             $link = 'http://youtu.be/' . $query;
-            var_dump($link);
             $title = $url->getTitle($link);
             $this->doPrivmsg($this->event->getSource(), $title);
             // $this->doPrivmsg($this->event->getSource(), 'Unable to find video info. Is it private? Brand new? (YouTube feeds don\'t update right away.)');
